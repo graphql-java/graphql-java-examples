7afa58c08e1a4752c7f8549bdeb0ff2a117630ec
Andreas Marek <amarek@atlassian.com>
2018-09-13 08:34:46 +1000

    server and client for defer started

---
+++ defer/server/src/main/java/com/graphqljava/defer/DeferApplication.java
@@ -0,0 +1,12 @@
+package com.graphqljava.defer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeferApplication.class, args);
+	}
+}
