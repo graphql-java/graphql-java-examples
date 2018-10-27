d12c423c8057b195881072376779bb3bd7a9a3df
Felipe Reis <felipe.gdr@gmail.com>
2018-10-27 20:57:56 +1100

    Replace response writer with simple map return

--- hibernate-example/src/main/java/com/graphql/java/hibernate/example/GraphQLController.java
+++ hibernate-example/src/main/java/com/graphql/java/hibernate/example/GraphQLController.java
@@ -3,7 +3,6 @@ package com.graphql.java.hibernate.example;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import graphql.ExecutionInput;
-import graphql.ExecutionResult;
 import graphql.GraphQL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -14,10 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -35,58 +31,51 @@ public class GraphQLController {
 
     @RequestMapping(value = "/graphql", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin
-    public void graphqlGET(@RequestParam("query") String query,
-                           @RequestParam(value = "operationName", required = false) String operationName,
-                           @RequestParam("variables") String variablesJson,
-                           HttpServletResponse httpServletResponse) throws IOException {
+    public Map<String, Object> graphqlGET(@RequestParam("query") String query,
+                                          @RequestParam(value = "operationName", required = false) String operationName,
+                                          @RequestParam("variables") String variablesJson) throws IOException {
         if (query == null) {
             query = "";
         }
+
         Map<String, Object> variables = new LinkedHashMap<>();
-        ;
+
         if (variablesJson != null) {
             variables = objectMapper.readValue(variablesJson, new TypeReference<Map<String, Object>>() {
             });
         }
-        executeGraphqlQuery(httpServletResponse, operationName, query, variables);
+
+        return executeGraphqlQuery(operationName, query, variables);
     }
 
     @SuppressWarnings("unchecked")
     @RequestMapping(value = "/graphql", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin
-    public void graphql(@RequestBody Map<String, Object> body, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+    public Map<String, Object> graphql(@RequestBody Map<String, Object> body) {
         String query = (String) body.get("query");
+
         if (query == null) {
             query = "";
         }
+
         String operationName = (String) body.get("operationName");
         Map<String, Object> variables = (Map<String, Object>) body.get("variables");
+
         if (variables == null) {
             variables = new LinkedHashMap<>();
         }
-        executeGraphqlQuery(httpServletResponse, operationName, query, variables);
+
+        return executeGraphqlQuery(operationName, query, variables);
     }
 
-    private void executeGraphqlQuery(HttpServletResponse httpServletResponse, String operationName, String query, Map<String, Object> variables) throws IOException {
+    private Map<String, Object> executeGraphqlQuery(String operationName,
+                                                    String query, Map<String, Object> variables) {
         ExecutionInput executionInput = ExecutionInput.newExecutionInput()
                 .query(query)
                 .variables(variables)
                 .operationName(operationName)
                 .build();
 
-        ExecutionResult executionResult = graphql.execute(executionInput);
-        handleNormalResponse(httpServletResponse, executionResult);
-    }
-
-    private void handleNormalResponse(HttpServletResponse httpServletResponse, ExecutionResult executionResult) throws IOException {
-        Map<String, Object> result = executionResult.toSpecification();
-        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
-        httpServletResponse.setCharacterEncoding("UTF-8");
-        httpServletResponse.setContentType("application/json");
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-        String body = objectMapper.writeValueAsString(result);
-        PrintWriter writer = httpServletResponse.getWriter();
-        writer.write(body);
-        writer.close();
+        return graphql.execute(executionInput).toSpecification();
     }
 }
